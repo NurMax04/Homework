911cb1a18479517af154a81306c60b0cca3d218e
NurMax04 <nurjgit94@gmail.com>
2026-05-31 17:19:35 +0500

    Add files via upload

---
+++ Session_3/main.js
@@ -0,0 +1,33 @@
+//                     Задача 1
+
+// for (let i = 1; i <= 20; i++) {
+// 	if (i % 4 === 0) {
+// 		continue;
+// 	}
+// 	console.log("Число который не делиться на 4 без остатка", i);
+// }
+
+//                     Задача 2
+
+// const number = +prompt("Введите любое число, мы найдем факториал (Х)!", 0);
+// let factorial = 1;
+// for (let i = 1; i <= number; i++) {
+// 	factorial *= i;
+// }
+// alert(`Факториал этого числа ${factorial}`);
+
+//                     Задача 3
+
+// let line = "";
+
+// for (let i = 1; i <= 8; i++) {
+// 	for (let j = 1; j <= 8; j++) {
+// 		if ((i + j) % 2 === 0) {
+// 			line += "#";
+// 		} else {
+// 			line += "_";
+// 		}
+// 	}
+// 	line += "\n";
+// }
+// console.log(line);
